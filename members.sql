8bd08ea0161921343a2735e17793b0e9f09177b4
CodexVic <ovajayi@gmail.com>
2020-02-01 22:44:15 +0100

    New Wine Admin Dashboard

---
+++ members.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Members (
+    membersId INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(200),
+    lastName VARCHAR(200),
+    dateOfBirth DATE,
+    contact VARCHAR (200),
+    email VARCHAR(200)
+    occupation VARCHAR (200),
+    unitDepartment VARCHAR (200),
+    position VARCHAR(200),
+    
+)
